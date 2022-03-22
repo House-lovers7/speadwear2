b36b1288367d9b75411dbcfb7f45cfeec2dd6347
House-lovers7 <taisuke.got@gmail.com>
2022-03-22 11:20:58 +0900

    #13 CordinateのFactoryファイルの作成

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Coordinate < ApplicationRecord
-  belongs_to :user, dependent: :destroy, presence: true
-  has_many :items, presence: true
+  belongs_to :user, dependent: :destroy
+  has_many :items
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
   has_many :likecoordinates, dependent: :destroy
@@ -11,18 +11,17 @@ class Coordinate < ApplicationRecord
   validates :season, presence: true
   validates :tpo, presence: true
   validates :memo, length: { maximum: 140 }
-  validate :picture_size
 
-  has_many :liked_coordinate, through: :likecoordinates, source: :coordinate
-  has_many :active_likecoordinates, class_name: 'Likecoordinate',
+  has_many :liked_coordinate, through: :likeCoordinates, source: :coordinate
+  has_many :active_likecoordinates, class_name: 'LikeCoordinate',
                                    foreign_key: 'coordinate_id',
                                    dependent: :destroy
 
   # liked_usersによってcoordinateが誰にいいねされているのかを簡単に取得できるようにする
-  has_many :liked_users, through: :likecoordinates, source: :user
+  has_many :liked_users, through: :likeCoordinates, source: :user
   mount_uploader :picture, PictureUploader
 
-  accepts_nested_attributes_for :items
+  # accepts_nested_attributes_for :items
   enum super_item: %w[アウター トップス ボトムス シューズ]
   enum season: %w[春 夏 秋 冬]
   enum tpo: %w[デート リラックス♪ スポーツ おでかけ 仕事]
